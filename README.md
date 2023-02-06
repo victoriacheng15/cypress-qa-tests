833b7d28d19189726b3fc49ff42994c62964467f
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-05 21:25:41 -0700

    Add google demo (#1)
    * feat: add a simple google demo
    
    * feat: add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cypress Tests
+
+WIP
---
+++ cypress/e2e/google-demos.cy.js
@@ -0,0 +1,18 @@
+describe("Google Homepage interaction", () => {
+	beforeEach(() => {
+		cy.visit("https://google.com");
+	});
+
+  it("should have sign in button", () => {
+    cy.get("a.gb_ha.gb_ia.gb_ee.gb_ed").should("contain", "Sign in");
+  })
+
+	it("should get input element and type Hello World", () => {
+		cy.get("input[type='text']").type("hello world")
+	})
+
+	it("should press enter after typed Hello World", () => {
+		cy.get("input[type='text']").type("hello world{enter}");
+		cy.url().should("contain", "hello+world")
+	})
+});
