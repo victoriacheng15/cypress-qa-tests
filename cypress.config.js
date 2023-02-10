baa75cd2f6aabb006e66f45b941bcbb7a2c0ffb4
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-10 12:38:42 -0700

    feat: add config to hide xhr (#4)

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   numTestsKeptInMemory: 30,
   e2e: {
+    hideXHRInCommandLog: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
